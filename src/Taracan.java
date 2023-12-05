139d7bb4ab89ae101a0e6b2f74dc9808fd1f86ca
nikelev <nikelev777@gmail.com>
2023-12-05 20:57:18 +0100

    commit 1

--- src/Taracan.java
+++ src/Taracan.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class Taracan {
+
+public class Taracan implements Runnable {
+    String tarakanName;
+    int runCycles;
+    int sleepTime;
+
+    public Taracan(String tarakanNumber, int runCycles, int sleepTime) {
+        this.tarakanName = tarakanNumber;
+        this.runCycles = runCycles;
+        this.sleepTime = sleepTime;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Tarakan #" + tarakanName + " is running");
+
+        for (int i = 0; i < runCycles; i++) {
+            System.out.println("Tarakan #" + tarakanName + " is running round number " + (i + 1));
+
+            try {
+                Thread.sleep(sleepTime);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        System.out.println("Tarakan #" + tarakanName + " finished");
+
+        TaracanAppl.setWinner(tarakanName);
+    }
 }
+
--- src/TaracanAppl.java
+++ src/TaracanAppl.java
@@ -1,2 +1,43 @@
-package PACKAGE_NAME;public class TaracanAppl {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class TaracanAppl {
+    static String winner = null;
+
+    public static void main(String[] args) throws InterruptedException {
+        int min = 2;
+        int max = 5;
+        int runCycles = 10;
+
+        Random random = new Random();
+        int sleepTime = min + random.nextInt((max + 1) - min);
+
+        System.out.println("Tarakans Race START!");
+
+        List<Thread> threads = new ArrayList<>();
+
+        for (int i = 0; i < 5; i++) {
+            threads.add(new Thread(new Taracan("Runner #"+i,runCycles,sleepTime)));
+        }
+
+        for (Thread thread : threads) {
+            thread.start();
+
+
+        }
+        for (Thread thread : threads) {
+
+            thread.join();
+
+        }
+
+        System.out.println("Tarakans Race FINISH!");
+        System.out.println("Congratulations to tarakan #" + winner + "!  It's a winner!");
+    }
+
+    public static void setWinner(String tarakanNumber) {
+        winner = winner == null ? tarakanNumber : winner;
+    }
+
 }
