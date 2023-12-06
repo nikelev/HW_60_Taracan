dd1c7955a947ec000bbc4c10ad3a3ac7b662a576
nikelev <nikelev777@gmail.com>
2023-12-06 08:31:13 +0100

    commit 4

--- src/Taracan.java
+++ src/Taracan.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Taracan implements Runnable {
@@ -14,9 +15,13 @@ public class Taracan implements Runnable {
     @Override
     public void run() {
         System.out.println("Tarakan #" + tarakanName + " is running");
+        int min = 2;
+        int max = 5;
+        Random random = new Random();
+        int sleepTime = min + random.nextInt((max + 1) - min);
 
         for (int i = 0; i < runCycles; i++) {
-            System.out.println("Tarakan #" + tarakanName + " is running round number " + (i + 1));
+            System.out.println("Tarakan #" + tarakanName + " is running round number " + (i + 1)+" SleepTime= "+sleepTime);
 
             try {
                 Thread.sleep(sleepTime);
